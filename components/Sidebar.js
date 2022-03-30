19ccef32aa7a781c951039b33cd2a6a3a5427ecf
qiang li <qxl0@yahoo.com>
2022-03-29 20:44:51 -0400

    Work on Sidebar

--- components/Sidebar.js
+++ components/Sidebar.js
@@ -1,11 +1,21 @@
-import { Avatar } from '@material-ui/core'
+import { Avatar, IconButton } from '@material-ui/core'
 import styled from 'styled-components'
+import ChatIcon from '@material-ui/icons/chat'
+import MoreVertIcon from '@material-ui/icons/MoreVert'
 
 const Sidebar = () => {
   return (
     <Container>
       <Header>
         <UserAvatar />
+        <IconsContainer>
+          <IconButton>
+            <ChatIcon />
+          </IconButton>
+          <IconButton>
+            <MoreVertIcon />
+          </IconButton>
+        </IconsContainer>
       </Header>
     </Container>
   )
@@ -15,6 +25,6 @@ export default Sidebar
 
 const Container = styled.div``
 const Header = styled.div``
-const UserAvatar = styled(Avatar)`
-  margin: 10px;
-`
+const UserAvatar = styled(Avatar)``
+
+const IconsContainer = styled.div``
