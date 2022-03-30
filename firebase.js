fc1334ad1a6cca4c0ebef4a73b5d3928c8f497ff
qiang li <qxl0@yahoo.com>
2022-03-29 21:06:32 -0400

    Add firebase support

---
+++ firebase.js
@@ -0,0 +1,17 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from 'firebase/app'
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: 'AIzaSyBenbVNQ6gXk_PrImxeVa9Qy4ZdRbNipPw',
+  authDomain: 'whatsapp-df8c5.firebaseapp.com',
+  projectId: 'whatsapp-df8c5',
+  storageBucket: 'whatsapp-df8c5.appspot.com',
+  messagingSenderId: '86568860322',
+  appId: '1:86568860322:web:071c758f88c5df7a5275fb',
+}
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig)
