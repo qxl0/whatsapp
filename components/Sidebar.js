496d63e2111f6540e6b10433d80ff8eeb096db65
qiang li <qxl0@yahoo.com>
2022-03-29 20:51:36 -0400

    WIP: Sidebar

--- components/Sidebar.js
+++ components/Sidebar.js
@@ -2,6 +2,7 @@ import { Avatar, IconButton } from '@material-ui/core'
 import styled from 'styled-components'
 import ChatIcon from '@material-ui/icons/chat'
 import MoreVertIcon from '@material-ui/icons/MoreVert'
+import SearchIcon from '@material-ui/icons/search'
 
 const Sidebar = () => {
   return (
@@ -17,6 +18,10 @@ const Sidebar = () => {
           </IconButton>
         </IconsContainer>
       </Header>
+
+      <Search>
+        <SearchIcon />
+      </Search>
     </Container>
   )
 }
@@ -24,7 +29,25 @@ const Sidebar = () => {
 export default Sidebar
 
 const Container = styled.div``
-const Header = styled.div``
-const UserAvatar = styled(Avatar)``
+const Search = styled.div``
+const Header = styled.div`
+  display: flex;
+  position: sticky;
+  top: 0;
+  background-color: white;
+  z-index: 1;
+  justify-content: space-between;
+  align-items: center;
+  padding: 15px;
+  height: 80px;
+  border-bottom: 1px solid whitesmoke;
+`
+const UserAvatar = styled(Avatar)`
+  cursor: pointer;
+
+  :hover {
+    opacity: 0.8;
+  }
+`
 
 const IconsContainer = styled.div``
