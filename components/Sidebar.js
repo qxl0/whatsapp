e36a33b502df419d16ae46dbde355a07bd13037c
qiang li <qxl0@yahoo.com>
2022-03-30 20:00:45 -0400

    Add Login

--- components/Sidebar.js
+++ components/Sidebar.js
@@ -3,8 +3,19 @@ import styled from 'styled-components'
 import ChatIcon from '@material-ui/icons/chat'
 import MoreVertIcon from '@material-ui/icons/MoreVert'
 import SearchIcon from '@material-ui/icons/search'
+import * as EmailValidator from 'email-validator'
 
 const Sidebar = () => {
+  const createChat = () => {
+    const input = prompt(
+      'Plse enter an email address for the user you wish to chat with'
+    )
+
+    if (!input) return null
+
+    if (EmailValidator.validate(input)) {
+    }
+  }
   return (
     <Container>
       <Header>
@@ -24,7 +35,7 @@ const Sidebar = () => {
         <SearchInput />
       </Search>
 
-      <SidebarButton>Start a new chat</SidebarButton>
+      <SidebarButton onClick={createChat}>Start a new chat</SidebarButton>
     </Container>
   )
 }
---
+++ pages/login.js
@@ -0,0 +1,15 @@
+import React from 'react'
+
+const login = () => {
+  return (
+    <Container>
+      <Head>
+        <title>Login</title>
+      </Head>
+    </Container>
+  )
+}
+
+export default login
+
+const Container = styled.div``
