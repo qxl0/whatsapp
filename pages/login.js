79c2cec48ce69356aae5eb318878fef8d2f7194c
qiang li <qxl0@yahoo.com>
2022-03-30 20:22:45 -0400

    Styling Login

--- pages/login.js
+++ pages/login.js
@@ -1,12 +1,18 @@
 import React from 'react'
 import Head from 'next/head'
 import styled from 'styled-components'
+import { Button } from '@material-ui/core'
 const Login = () => {
   return (
     <Container>
       <Head>
         <title>Login</title>
       </Head>
+
+      <LoginContainer>
+        <Logo src="https://assets.stickpng.com/images/580b57fcd9996e24bc43c543.png" />
+        <Button>Sign in with Google</Button>
+      </LoginContainer>
     </Container>
   )
 }
@@ -14,3 +20,10 @@ const Login = () => {
 export default Login
 
 const Container = styled.div``
+
+const LoginContainer = styled.div``
+const Logo = styled.img`
+  height: 200px;
+  width: 200px;
+  margin-bottom: 50px;
+`
