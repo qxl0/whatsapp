71c0168f64516d18f52342d6f8121c23f4b5c927
qiang li <qxl0@yahoo.com>
2022-03-30 20:50:26 -0400

    Loading page

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -3,10 +3,11 @@ import type { AppProps } from 'next/app'
 import { useAuthState } from 'react-firebase-hooks/auth'
 import { auth, db } from '../firebase'
 import Login from './login'
-
+import Loading from '../components/Loading'
 function MyApp({ Component, pageProps }: AppProps) {
-  const [user] = useAuthState(auth)
+  const [user, loading] = useAuthState(auth)
 
+  if (true) return <Loading />
   if (!user) return <Login />
   return <Component {...pageProps} />
 }
