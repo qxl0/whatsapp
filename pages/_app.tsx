fc9a4ff8809b8a396c3018a3ea5c514aa6266ff5
qiang li <qxl0@yahoo.com>
2022-03-30 20:58:35 -0400

    Save user info to fireStore

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -4,9 +4,25 @@ import { useAuthState } from 'react-firebase-hooks/auth'
 import { auth, db } from '../firebase'
 import Login from './login'
 import Loading from '../components/Loading'
+import { useEffect } from 'react'
+import { doc, serverTimestamp, setDoc } from 'firebase/firestore'
+
 function MyApp({ Component, pageProps }: AppProps) {
   const [user, loading] = useAuthState(auth)
 
+  useEffect(() => {
+    if (user) {
+      setDoc(
+        doc(db, 'users', user.uid),
+        {
+          email: user.email,
+          lastSeen: serverTimestamp(),
+          photoURL: user.photoURL,
+        },
+        { merge: true }
+      )
+    }
+  }, [user])
   if (loading) return <Loading />
   if (!user) return <Login />
   return <Component {...pageProps} />
