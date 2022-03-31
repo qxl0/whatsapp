c6784eb20c46db9b53109863be93b7354a8b2e77
qiang li <qxl0@yahoo.com>
2022-03-30 20:31:41 -0400

    Sign in with callback to redirect to home page after siginin

--- pages/login.js
+++ pages/login.js
@@ -2,7 +2,12 @@ import React from 'react'
 import Head from 'next/head'
 import styled from 'styled-components'
 import { Button } from '@material-ui/core'
+import { auth, provider } from '../firebase'
+import { signInWithPopup } from 'firebase/auth'
 const Login = () => {
+  const signIn = () => {
+    signInWithPopup(auth, provider).catch(alert)
+  }
   return (
     <Container>
       <Head>
@@ -11,7 +16,7 @@ const Login = () => {
 
       <LoginContainer>
         <Logo src="https://assets.stickpng.com/images/580b57fcd9996e24bc43c543.png" />
-        <Button>Sign in with Google</Button>
+        <Button onClick={signIn}>Sign in with Google</Button>
       </LoginContainer>
     </Container>
   )
