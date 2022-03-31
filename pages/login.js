34cb9c9728ea6227d5387530e7db832ccbadc779
qiang li <qxl0@yahoo.com>
2022-03-30 20:26:33 -0400

    Nice Sign in page

--- pages/login.js
+++ pages/login.js
@@ -19,9 +19,22 @@ const Login = () => {
 
 export default Login
 
-const Container = styled.div``
+const Container = styled.div`
+  display: grid;
+  place-items: center;
+  height: 100vh;
+  background-color: whitesmoke;
+`
 
-const LoginContainer = styled.div``
+const LoginContainer = styled.div`
+  padding: 100px;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  background-color: white;
+  border-radius: 5px;
+  box-shadow: 0px 4px 14px -3px rgba(0, 0, 0, 0.7);
+`
 const Logo = styled.img`
   height: 200px;
   width: 200px;
