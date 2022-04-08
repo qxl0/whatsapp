f3d26cf0d4b39fa9e3a96de5e69c68d9a5eef19d
qiang li <qxl0@yahoo.com>
2022-04-07 21:08:35 -0400

    Serverside reder messages

--- pages/chat/[id].js
+++ pages/chat/[id].js
@@ -3,6 +3,7 @@ import Head from 'next/head'
 import styled from 'styled-components'
 import Sidebar from '../../components/Sidebar'
 import ChatScreen from '../../components/ChatScreen'
+import { orderBy, query, collection, getDoc } from 'firebase/firestore'
 
 const Chat = () => {
   return (
@@ -20,6 +21,29 @@ const Chat = () => {
 
 export default Chat
 
+export async function getServerSideProps(context) {
+  const ref = query(
+    collection(db, 'chats', context.query.id),
+    orderBy('timestamp', 'asc')
+  )
+  const messagesRes = await getDoc(ref)
+  messagesRes.docs
+    .map((doc) => ({
+      id: doc.id,
+      ...doc.data(),
+    }))
+    .map((messages) => ({
+      ...messages,
+      timestamp: messages.timestamp.toDate().getTime(),
+    }))
+
+  // PREF the chat
+  const chatRes = await getDoc(ref)
+  const chat = {
+    id: chatRes.id,
+    ...chatRes.data(),
+  }
+}
 const Container = styled.div`
   display: flex;
 `
