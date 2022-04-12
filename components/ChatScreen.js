3a58e8281952d7a96a7cb2fdf1e6b20b38258774
qiang li <qxl0@yahoo.com>
2022-04-11 20:49:56 -0400

    Styled message container

--- components/ChatScreen.js
+++ components/ChatScreen.js
@@ -119,4 +119,8 @@ const HeaderIcons = styled.div``
 
 const EndOfMessage = styled.div``
 
-const MessageContainer = styled.div``
+const MessageContainer = styled.div`
+  padding: 30px;
+  background-color: #e5ded8;
+  min-height: 90vh;
+`
