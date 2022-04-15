4c9f34814ef969a6365e4ee6a049f58ef44744bf
qiang li <qxl0@yahoo.com>
2022-04-14 21:01:47 -0400

    Add sender/receiver message

--- components/Message.js
+++ components/Message.js
@@ -1,10 +1,16 @@
 import React from 'react'
+import { useAuthState } from 'react-firebase-hooks/auth'
 import styled from 'styled-components'
+import { auth } from '../firebase'
 
 const Message = ({ user, message }) => {
+  const [userLoggedIn] = useAuthState(auth)
+
+  const TypeOfMessage = user === userLoggedIn.email ? Sender : Receiver
+
   return (
     <Container>
-      <p>{message.message}</p>
+      <TypeOfMessage>{message.message}</TypeOfMessage>
     </Container>
   )
 }
@@ -12,3 +18,23 @@ const Message = ({ user, message }) => {
 export default Message
 
 const Container = styled.div``
+
+const MessageElement = styled.p`
+  width: fit-content;
+  padding: 15px;
+  border-radius: 8px;
+  margin: 10px;
+  min-width: 60px;
+  padding-bottom: 26px;
+  position: relative;
+  text-align: right;
+`
+
+const Sender = styled(MessageElement)`
+  margin-left: auto;
+  background-color: #dcf8c6;
+`
+const Reciver = styled(MessageElement)`
+  background-color: whitesmoke;
+  text-align: left;
+`
